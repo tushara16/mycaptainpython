69fdd327d8fa4568a264a61ad82dd5fde356a9cc
tushara16 <67357920+tushara16@users.noreply.github.com>
2021-12-20 19:53:48 +0530

    Add files via upload

---
+++ assignment1.1.py
@@ -0,0 +1,51 @@
+a=20
+b=19
+c=18
+# logical operators
+# and operator
+if(a>b and a>c):
+ print("a is greater")
+elif(b>a and b>c):
+ print("b is greater")
+else:
+ print("c is greater")
+
+#or operator
+d=13
+e=30
+f=45
+if(d>e or d>f):
+  print("d is greater")
+elif(e>d or e>f):
+  print("e is greater")
+else:
+  print("f is greater")
+
+  #not
+if not a> 10:
+   print("not returned True")
+else:
+   print("not retured False")
+
+ # comparison operators
+ # >,<
+g=25
+h=45
+if(g>h):
+     print("g is greater")
+else:
+    print("h is greater")
+# ==,!=
+i=24
+j=45
+if(i==j):
+    print("i is equal to j")
+else:
+    print("i is not equal to j")
+# >=,<=
+k=34
+l=90
+if(k>=l):
+    print("k is greater than or equal to l")
+else:
+    print("k is less than or equal to l")
