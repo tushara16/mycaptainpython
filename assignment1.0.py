6458ddd7e757d1743dec194a7f8c565f4c1ad742
tushara16 <67357920+tushara16@users.noreply.github.com>
2021-12-20 22:13:33 +0530

    Add files via upload

---
+++ assignment1.0.py
@@ -0,0 +1,17 @@
+#10,1,8,3,6,5,4,7,x,y
+# x value is 2 and y is 9
+
+
+#floor division
+a=100
+b=20
+print(a//b)
+c=10
+d=3
+print(c//d)
+
+from math import floor
+print(floor(a//b))
+e=120
+f=55
+print(floor(e//f))
